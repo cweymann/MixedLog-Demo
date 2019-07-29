f5608e238272b265ec92070b7826ca6e0e868b26
Claus Weymann <c.weymann@avm.de>
2019-07-29 16:22:44 +0200

    configured as framework while dependecy is static library

--- Example/setup.sh
+++ Example/setup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-DIRECTORY="../../MixedErrorLog"
-if [ ! -d "$DIRECTORY" ]; then
-	git clone https://github.com/cweymann/MixedErrorLog-Demo.git "$DIRECTORY"
-fi
-
-pod install
-open MixedLog.xcworkspace
+./setupAsStatic.sh
+# ./setupAsFramework.sh
